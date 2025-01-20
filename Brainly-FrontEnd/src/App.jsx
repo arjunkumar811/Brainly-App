ec90ea640fa57e3bc15d4d81cb9502444286451f
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-20 22:26:18 +0530

    js

--- Brainly-FrontEnd/src/App.jsx
+++ Brainly-FrontEnd/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { SignUp } from './pages/SignUp';
+import  SignUp  from '/src/pages/SignUp';
+
 
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { SignIn } from './pages/SignIn';
+import Signin from './pages/SignIn';
 import { DashBoard } from './pages/dashboard';
 import Homepage from './components/Homepage';
 import { Features } from './components/Features';
@@ -19,7 +20,7 @@ return <BrowserRouter>
 
 <Route path='/' element={<Homepage />} />
 <Route path='/signup' element={<SignUp />} />
-<Route path='/signin' element={<SignIn />} />
+<Route path='/signin' element={<Signin />} />
 <Route path='dashboard' element={<DashBoard />} />
 <Route path="/features" element={<Features />} />
 </Routes>
